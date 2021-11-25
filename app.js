223937de35c4e23ee9f774f8dbb4f8a1f00a21b8
Tomohiro-Akinaga <55231048+Tomohiro-Akinaga@users.noreply.github.com>
2021-11-24 16:44:21 -0800

    modified

--- app.js
+++ app.js
@@ -1,33 +1,74 @@
 const APIURL =
-    "https://api.themoviedb.org/3/discover/movie?api_key=9a488bdcdb8993c3fff35cd18757981d&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1";
-
+    "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=9a488bdcdb8993c3fff35cd18757981d&page=1";
 const IMGPATH = "https://image.tmdb.org/t/p/w1280";
+const SEARCHAPI =
+    "https://api.themoviedb.org/3/search/movie?&api_key=9a488bdcdb8993c3fff35cd18757981d&query=";
 
-async function getMovies() {
-    const resp = await fetch(APIURL);
+const main = document.getElementById("main");
+const form = document.getElementById("form");
+const search = document.getElementById("search");
+
+// initially get fav movies
+getMovies(APIURL);
+
+async function getMovies(url) {
+    const resp = await fetch(url);
     const respData = await resp.json();
 
     console.log(respData);
 
-    respData.results.forEach(movie => {
-         const { poster_path, title, vote_average} = movie;
-        
-         const movieEl = document.createElement("div");
-         movieEl.classList.add("movie");
-
-         movieEl.innerHTML = `<img
-                                 src="${IMGPATH + poster_path}"
-                                 alt="${title}"
-                             />
-                             <div class="movie-info">
-                                 <h3>${title}</h3>
-                                 <span>${vote_average}</span>
-                             </div>`;
-
-        document.body.appendChild(movieEl);
-     });
-
-    return respData;
+    showMovies(respData.results);
 }
 
-getMovies();
+function showMovies(movies) {
+    // clear main
+    main.innerHTML = "";
+
+    movies.forEach((movie) => {
+        const { poster_path, title, vote_average, overview } = movie;
+
+        const movieEl = document.createElement("div");
+        movieEl.classList.add("movie");
+
+        movieEl.innerHTML = `
+            <img
+                src="${IMGPATH + poster_path}"
+                alt="${title}"
+            />
+            <div class="movie-info">
+                <h3>${title}</h3>
+                <span class="${getClassByRate(
+                    vote_average
+                )}">${vote_average}</span>
+            </div>
+            <div class="overview">
+                <h3>Overview:</h3>
+                ${overview}
+            </div>
+        `;
+
+        main.appendChild(movieEl);
+    });
+}
+
+function getClassByRate(vote) {
+    if (vote >= 8) {
+        return "green";
+    } else if (vote >= 5) {
+        return "orange";
+    } else {
+        return "red";
+    }
+}
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const searchTerm = search.value;
+
+    if (searchTerm) {
+        getMovies(SEARCHAPI + searchTerm);
+
+        search.value = "";
+    }
+});
