67d1c374b1beb9604e368da864fdbd5fb97b9fdb
Tomohiro-Akinaga <55231048+Tomohiro-Akinaga@users.noreply.github.com>
2021-11-26 16:04:01 -0800

    修正

--- javascript.js
+++ javascript.js
@@ -4,7 +4,6 @@
 
 
 //  Function of fetching the data
-
 fetchData("https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=9a488bdcdb8993c3fff35cd18757981d&page=1");
 
 async function fetchData(url) {
@@ -20,12 +19,12 @@ function displayData(dataInfo) {
     main.innerHTML = "";
 
     dataInfo.forEach((movie) => {
-        const image = movie.poster_path;
+        const image = movie.backdrop_path;
         const title = movie.original_title;
         
-        const movieEl = document.createElement("div");
-        movieEl.classList.add("movie");
-        movieEl.innerHTML = `
+        const movieItem = document.createElement("div");
+        movieItem.classList.add("movie");
+        movieItem.innerHTML = `
             <a href="#" id="link"> 
             <img src="${"https://image.tmdb.org/t/p/original" + image}" />
             <div class="movie-info">
@@ -33,19 +32,28 @@ function displayData(dataInfo) {
             </div>
             </a>
             `;
-        main.appendChild(movieEl);        
+        main.appendChild(movieItem);
+
+        // When the item is clicked
+        const a = document.getElementById("link");
+        a.addEventListener("click", displayDesc);
+        function displayDesc() {
+            // movieItem.innerHTML = `<p>${overview}<p>`
+            console.log(movieItem);
+        }
     });
 };
 
-// When the item is clicked
+
+
+// When the search bottun is clicked
 const form = document.getElementById("form");
 form.addEventListener("submit", (event) => {
     event.preventDefault();
     const search = document.getElementById("search");
     const keyword = search.value;
-    if (keyword) {
-        fetchData("https://api.themoviedb.org/3/search/movie?&api_key=9a488bdcdb8993c3fff35cd18757981d&query=spiderman");
-        search.value = "";
-    }
+    fetchData("https://api.themoviedb.org/3/search/movie?&api_key=9a488bdcdb8993c3fff35cd18757981d&query=" + keyword);
+    search.value = "";
 });
+
 
